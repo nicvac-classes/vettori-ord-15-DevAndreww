cc1d7686b25f4270f1168f4d7c0faddbe1f0429a
DevAndreww <andrea.sette2@marconibari.edu.it>
2025-04-02 17:50:46 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    srand(time(0));  
+    
+    int n, i, j;
+    int t;
+    string s;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout << "Quante auto percorrono la corsa?" << endl;
+    cin >> n;
+    int r[n];
+    string nomi[n];
+
+    i = 0;
+    t = 0;
+    while (i < n) {
+        cout << "Digita il " << i + 1 << "° nome." << endl;
+        cin >> nomi[i];
+        r[i] = 1 + rand() % (n + 1);
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        j = 0;
+        while (j <= n - 2 - i) {
+            if (r[j] > r[j + 1]) {
+                t = r[j];
+                r[j] = r[j + 1];
+                r[j + 1] = t;
+                s = nomi[j];
+                nomi[j] = nomi[j + 1];
+                nomi[j + 1] = s;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        cout << i + 1 << "° nome: " << nomi[i] << ", posizione: " << r[i] << "." << endl;
+        i = i + 1;
+    }
+    
+}
